34b97f027a1e487b973927a9c213225d9dc1c8c5
Morten V. Pedersen <morten@mortenvp.com>
2025-06-04 21:04:34 +0200

    Initial commit

--- test/main.cpp
+++ test/main.cpp
@@ -6,6 +6,7 @@
 #include <cstdint>
 #include <ctime>
 
+#include <google/protobuf/stubs/common.h>
 #include <gtest/gtest.h>
 
 int main(int argc, char** argv)
@@ -13,5 +14,10 @@ int main(int argc, char** argv)
     srand(static_cast<uint32_t>(time(0)));
 
     testing::InitGoogleTest(&argc, argv);
+
+    // Delete all global objects allocated by libprotobuf. Otherwise Valgrind
+    // will complain about memory leaks.
+    google::protobuf::ShutdownProtobufLibrary();
+
     return RUN_ALL_TESTS();
 }
